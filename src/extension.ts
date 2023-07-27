d2399b1dc001ee81d19b481a1618254b1c2fd09c
Anna Cardon <t-annacardon@microsoft.com>
2023-07-26 17:01:16 -0700

    testing connection from local to remote

--- src/extension.ts
+++ src/extension.ts
@@ -8,6 +8,7 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
+	//test of connection
 	console.log('Congratulations, your extension "annaPrototype" is now active!');
 
 	// The command has been defined in the package.json file
