40e99822bc3179b8a3b3e37238f80c5d9132e8ef
Anna Cardon <t-annacardon@microsoft.com>
2023-07-28 23:09:28 -0700

    Added throw new error so that if the oras path doesn't exist then the command doesn't run and user gets a pop up error

--- src/commands/showReferrer.ts
+++ src/commands/showReferrer.ts
@@ -1,10 +1,10 @@
 import { TaskCommandRunnerFactory } from '../support/TaskCommandRunnerFactory';
+import { window } from 'vscode';
 import * as os from 'os';
 import * as path from 'path';
 import { access } from 'fs/promises'; //this import accepts one argument for calling access
 
 export async function showReferrer(imageTag: any): Promise<void> {
-
     try {
         const taskCRF = new TaskCommandRunnerFactory({
             taskName: 'oras', // Specify the task name here
@@ -12,35 +12,34 @@ export async function showReferrer(imageTag: any): Promise<void> {
             focus: true, // Optional: Specify whether to focus on the task's output panel in the UI, currently true for now and can change later
         });
 
-    function getOrasPath() {
-        // check oras exist
+        async function getOrasPath() {
+            // check oras exist on user computer
+            const isWindows = os.platform() === 'win32'; //identifying if its a windows platform using os.plateform, if it is then isWindows will be true
+            const homedir = os.homedir(); //getting the host computer home directory
+            const orasExecutable = isWindows ? 'oras.exe' : 'oras'; //switches use of executable files. if true use oras.exe and if false use oras
+            const dirOras = path.join(homedir, 'bin', 'oras', orasExecutable); //returns the path to oras
 
-        const isWindows = os.platform() === 'win32'; //identifying if its a windows platform using process.plateform from Node.js, if it is then isWindows will be true
-        const homedir = os.homedir(); //getting the host computer home directory
-        const orasExecutable = isWindows ? 'oras.exe' : 'oras'; //switches use of executable files. if true use oras.exe and if false use oras
-        const dirOras = path.join(homedir, 'bin', 'oras', orasExecutable); //returns the path to oras
+            try {
+                await access(dirOras); // Check if the file exists, this will throw an error if the file doesn't exist
+                return dirOras; // If the file exists, return the dirOras path
+            } catch (err) {
+                throw new Error('ORAS executable not found.'); // Throw a custom error if ORAS executable doesn't exist
+            }
+        }
+        const orasPath = await getOrasPath();
 
-        try {
-            access(dirOras); // Check if the file exists, this will throw an error if the file doesn't exist
-            return dirOras; // If the file exists, return the dirOras path
-          } catch (err) {
-            console.log('no oras file found');
-            return ' '; // If the file doesn't exist, return a empty string
-          }
-    }
-        const orasPath = getOrasPath();
-
-       const validTag = /^[a-z0-9./_:-]+$/; //Repository names can only include lowercase alphanumeric characters, periods, dashes, underscores, and forward slashes. But adding a Tag gives it a colon
-       if(validTag.test(imageTag.fullTag)){
-        await taskCRF.getCommandRunner()({
-            command: orasPath,            //'C:\\Users\\t-annacardon\\bin\\oras\\oras.exe', //TODO: eventually fix, but currently has the path to my local oras
-            args : ['discover', imageTag.fullTag]
-        });
+        const validTag = /^[a-z0-9./_:-]+$/; //Repository names can only include lowercase alphanumeric characters, periods, dashes, underscores, and forward slashes. But adding a Tag gives it a colon
+        if(validTag.test(imageTag.fullTag)){
+            await taskCRF.getCommandRunner()({
+                command: orasPath,            
+                args : ['discover', imageTag.fullTag]
+            });
        }
-       else{
-        console.log("invalid tag");
+        else{
+            window.showErrorMessage('Invalid Tag provided');
        }
     } catch (error) {
+        window.showErrorMessage('An error occurred: ' + error);
         console.error('Error executing command:', error);
     }
 }
